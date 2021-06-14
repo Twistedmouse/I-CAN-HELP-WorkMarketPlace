7d2fc0536304649a0a093d19268bd89a016a1378
SBC <lochan.sharad@gmail.com>
2021-06-14 21:32:25 +0800

    added path for homepage in server.js file.

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,3 +1,58 @@
+// const loginFormHandler = async (event) => {
+//     event.preventDefault();
+
+//     const email = document.querySelector('#email-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
+
+//     if (email && password) {
+//       const response = await fetch('/api/users/login', {
+//         method: 'POST',
+//         body: JSON.stringify({ email, password }),
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+
+//       if (response.ok) {
+//         document.location.replace('/');
+//       } else {
+//         alert('Failed to log in.');
+//       }
+//     }
+//   };
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const firstname = document.querySelector('#firstname').value.trim();
+    const lastname = document.querySelector('#lastname').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (firstname && lastname && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ firstname, lastname, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            console.log("User SignUp complete.");
+            // document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+//   document
+//     .querySelector('.login-form')
+//     .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+
+
+
 // TODO: login page
 // WHEN: input email and Password
 // IF: does not exist in db prompt to use the sign up button or
--- server.js
+++ server.js
@@ -28,11 +28,18 @@ const PORT = process.env.PORT || 3001;
 // app.engine('handlebars', hbs.engine);
 // app.set('view engine', 'handlebars');
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.use(routes);
+
+
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
