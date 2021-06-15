f913dcd2e333daa976608b25fb1eef86a2456a76
tristan <mousy93@hotmail.com>
2021-06-15 12:07:35 +0800

    issue with User model
    iv removed the  { } from const User and that seemed to fix the undefind value if this is an issue we can revert

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models/User");
+const User = require("../../models");
 console.log("UserModel", User);
 // CREATE new user
 router.post("/", async (req, res) => {
