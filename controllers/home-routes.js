bbb9b252c943c17d7c89ec15b4bf4369a4a9b0e3
SBC <lochan.sharad@gmail.com>
2021-06-19 13:37:00 +0800

    session to check user logged in status added. Only logged in users can access the application.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,71 +7,74 @@ const { Job, User } = require("../models");
 
 //GET all jobs for homepage
 router.get('/', async (req, res) => {
-    try {
-        const allJobs = await Job.findAll({
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        try {
+            const allJobs = await Job.findAll({
 
-            order: [
-                ['date', 'DESC'],
-            ],
-            include: [{
-                model: User,
-                attributes: ['first_name', 'last_name', 'email'],
-            }],
+                order: [
+                    ['date', 'DESC'],
+                ],
+                include: [{
+                    model: User,
+                    attributes: ['first_name', 'last_name', 'email'],
+                }],
 
-        });
+            });
 
-        const jobs = allJobs.map((job) =>
-            job.get({ plain: true })
-        );
+            const jobs = allJobs.map((job) =>
+                job.get({ plain: true })
+            );
 
-        res.render('homepage', { jobs });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+            res.render('homepage', { jobs });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
     }
-
 });
 
 router.get('/job/:id', async (req, res) => {
-    // If the user is not logged in, redirect the user to the login page
-    // if (!req.session.loggedIn) {
-    //   res.redirect('/login');
-    // } else {
-    // If the user is logged in, allow them 
+    // 'If the user is not logged in, redirect the user to the login page
 
-    try {
-        const allJobs = await Job.findByPk(req.params.id, {
-            include: [{
-                model: User,
-                attributes: ['first_name', 'last_name', 'email'],
-            }],
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        try {
+            const allJobs = await Job.findByPk(req.params.id, {
+                include: [{
+                    model: User,
+                    attributes: ['first_name', 'last_name', 'email'],
+                }],
 
-        });
+            });
 
-        const job = allJobs.get({ plain: true });
-        console.log(job);
-        res.render('job', { job });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error);
+            const job = allJobs.get({ plain: true });
+            console.log(job);
+            res.render('job', { job });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
     }
 });
 
 
 router.get("/login", (req, res) => {
-    //   if (req.session.loggedIn) {
-    //     res.redirect("/");
-    //     return;
-    //   }
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
 
     res.render("login");
 });
 
 router.get("/signUp", (req, res) => {
-    // if (req.session.loggedIn) {
-    //     res.redirect('/homepage');
-    //     return;
-    // }
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
 
     res.render("signUp");
 });
@@ -79,9 +82,11 @@ router.get("/signUp", (req, res) => {
 
 //Create New Job
 router.get('/postjob', async (req, res) => {
-
-    res.render('postjob');
-
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    } else {
+        res.render('postjob');
+    }
 });
 
 
